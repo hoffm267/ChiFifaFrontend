02ac3c47cebe7736a892a73258fe956a42de34e1
Alexander Hoffmann <ahoffmann@spotinc.com>
2022-06-29 15:56:39 -0400

    starting icon work

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { FaVolleyballBall, FaChrome } from 'react-icons/fa';
+
+interface IProps {
+	color: string;
+	icon:  React.ReactElement<any, any>;
+}
 
 const NavBar = () => {
 	return (
 		<div className="fixed flex flex-row h-16 w-screen bg-gray shadow-xl ">
-
-			<SideBarIcon />
+			<SideBarIcon color="dollarBill" icon={<FaVolleyballBall />} />
 			<div
 				className="    	
                  italic text-6xl font-bold "
@@ -13,33 +18,19 @@ const NavBar = () => {
 				<span className="text-snow">FI</span>
 				<span className="text-red">FA</span>
 			</div>
-			<SideBarIcon1 />
-
+			<SideBarIcon color="red" icon={<FaChrome />} />
 		</div>
 	);
 };
 
-const SideBarIcon = () => (
-	<div
-		className="  
-               h-10 w-10  mx-auto my-auto
-               bg-dollarBill text-snow
-               
-               rounded-3xl hover:rounded-xl
-               transition-all duration-300 ease-linear
-               cursor-pointer"
-	></div>
-);
-const SideBarIcon1 = () => (
-	<div
-		className="   
-               h-10 w-10  mx-auto my-auto
-               bg-red text-snow
-               rounded-3xl hover:rounded-xl
-               transition-all duration-300 ease-linear
-               cursor-pointer"
-	></div>
-);
+const SideBarIcon = (props: IProps) => {
+	const { color, icon } = props;
+	return (
+		<div
+			className={`h-10 w-10  mx-auto my-auto bg-${color} flex items-center text-snow rounded-3xl hover:rounded-xl transition-all duration-300 ease-linear cursor-pointer`}
+		>{icon}</div>
+	);
+};
 const SideBarLogo = () => <div className="fixed "></div>;
 
 export default NavBar;
