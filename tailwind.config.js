6329940fda23b4458b6353603f0d97767c8abc22
Alexander Hoffmann <ahoffmann@spotinc.com>
2022-06-29 12:42:05 -0400

    initial website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+//#E33E33 other red
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'black': '#000805',
+      'red': '#F05D5E',
+      'dollarBill': '#97B85D',
+      'oliveDrab': '#668925',
+      'gray': '#2B373B',
+      'purple': '#776871',
+      'snow': "#fffafa"
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
